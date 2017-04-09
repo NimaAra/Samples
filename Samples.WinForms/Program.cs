dc3958aab83d4abaa5d262ee49f0f6047a284762
Nima Ara <Nima.Ara@outlook.com>
2017-04-09 02:43:15 +0100

    Data grid example working.

--- Samples.WinForms/Program.cs
+++ Samples.WinForms/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace Samples.WinForms
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Forms;
 
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            using (var form = new Form())
+            using (var grid = new DataGridView())
+            using (var tickerManager = new TickerManager())
+            {
+                grid.DataSource = new BindingList<Ticker>(tickerManager.Tickers);
+                grid.Dock = DockStyle.Fill;
+
+                form.Controls.Add(grid);
+                Application.Run(form);
+            }
         }
     }
 }
